b5226e463a0a59ef58e9ab5827ba275cbb17d3a8
schrodinger-c <jul14-p@proton.me>
2024-08-06 15:49:47 -0300

    first commit

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Companies.css';
+
+const Companies = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth flexCenter c-container">
+                <img src="../../../public/prologis.png" alt="prologis" />
+                <img src="../../../public/tower.png" alt="tower" />
+                <img src="../../../public/equinix.png" alt="equinix" />
+                <img src="../../../public/realty.png" alt="realty" />
+            </div>
+        </section>
+    )
+}
+
+<img src="../../../public/logo.png" alt="logo" width={100} />
+
+
+export default Companies;
